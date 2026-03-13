f67c3c246064c2a89eafb8e7becae9e666606b85
Mostafa Rabea <m.hasheem@nu.edu.eg>
2026-03-13 18:02:01 +0200

    Add module2-solution for Assignment 2

---
+++ assignments/assignment2/module2-solution/app.js
@@ -0,0 +1,53 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+  toBuy.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var toBuyItems = [
+    { name: "cookies", quantity: 10 },
+    { name: "chips", quantity: 5 },
+    { name: "soda bottles", quantity: 2 },
+    { name: "apples", quantity: 6 },
+    { name: "bananas", quantity: 8 }
+  ];
+  var boughtItems = [];
+
+  service.buyItem = function (itemIndex) {
+    var item = toBuyItems[itemIndex];
+    toBuyItems.splice(itemIndex, 1);
+    boughtItems.push(item);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
